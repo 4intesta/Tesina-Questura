af345821bd6de87fd0ead00eaf1d587db4405fd8
DarioFerrari-git <257661@studenti.unimore.it>
2021-05-16 11:10:24 +0200

    push ultimo

--- Tesina3/src/main/java/Tiw/Tesina3/Generale.java
+++ Tesina3/src/main/java/Tiw/Tesina3/Generale.java
@@ -1,5 +1,45 @@
 package Tiw.Tesina3;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.Filter;
+import com.google.appengine.api.datastore.Query.FilterOperator;
+import com.google.appengine.api.datastore.Query.FilterPredicate;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
 public class Generale {
 
+	private UserService userService;
+	private DatastoreService ds;
+	
+	public Generale () {
+		userService = UserServiceFactory.getUserService();
+		ds = DatastoreServiceFactory.getDatastoreService();
+	}
+	
+	public static ArrayList<String> longScuole() {
+		DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
+		Query q = new Query("SCUOLE CREATE");
+		List<Filter> filters = new ArrayList<Filter>();
+		Filter f = new FilterPredicate("longitudine",FilterOperator.NOT_EQUAL,"ZZZ");
+		q.setFilter(f);
+		PreparedQuery pq = ds.prepare(q);
+		List<Entity> list = pq.asList(FetchOptions.Builder.withLimit(300));
+		
+		StringBuffer sb = new StringBuffer();
+		ArrayList<String> a = new ArrayList<String> (); 
+		for (Entity e : list) {
+			a.add(e.getProperty("longitudine").toString());
+		}
+		return a;
+	
+}
 }
--- Tesina3/src/main/webapp/Mappe.jsp
+++ Tesina3/src/main/webapp/Mappe.jsp
@@ -7,7 +7,7 @@
  <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
    integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
    crossorigin=""></script>
-
+<%@page import="Tiw.Tesina3.Generale" %>
    <style>
    #mapid { height: 1000px; }
    </style>
@@ -18,14 +18,20 @@
 
 </body>
  <script>
+
  var mymap = L.map('mapid').setView([44.781811, 10.854439], 14);
  L.tileLayer('https://api.maptiler.com/maps/toner/{z}/{x}/{y}.png?key=GYJ2vr5kpPhplU9c4Lja',{
 	 attribution:'<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
 	 
-	 
-	 
  }).addTo(mymap);
- 
+ <% 
+ Generale g=new Generale();
+ var circle= L.circle([10,10], {
+	 color:'red',
+	 fillColor:'#f03',
+	 radius:10
+ }).addTo(mymap);
+ %>
  </script>
 </html>
 
